17b5bb20452c426f6a07b91107946d3b4dea102c
Subham-Jaiswar <subhamjaiswar3@gmail.com>
2023-05-22 13:59:19 +0530

    first commit

---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,15 @@
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh" }}>
+      <PrettyChatWindow
+        projectId="255288a1-b086-48bc-84e7-9a1c7ec08d91"
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+export default ChatsPage;
